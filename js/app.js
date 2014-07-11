80204f1866cde691ca3f2b8c91b98b8b7afb6cfe
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-11 17:04:38 +0100

    add ngClipboard and add a button to copy the generated link to the clipboard

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Main application
-angular.module('MatchCalendar', ['ui.bootstrap', 'ngCookies', 'ngSanitize', 'btford.markdown', 'ui.router'])
+angular.module('MatchCalendar', ['ui.bootstrap', 'ngCookies', 'ngSanitize', 'btford.markdown', 'ui.router', 'ngClipboard'])
 
     //configuration
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {

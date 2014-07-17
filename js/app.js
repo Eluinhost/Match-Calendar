08bb516972b9d37d25177b491a91f600385759e7
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-17 12:18:37 +0100

    service support for multiple subreddits

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ angular.module('MatchCalendar', ['ui.bootstrap', 'ngCookies', 'ngSanitize', 'btf
 
         $scope.posts = [];
         $scope.updatePosts = function() {
-            RedditPostsService.query().then(function(data) {
+            RedditPostsService.query(['ultrahardcore']).then(function(data) {
                 $scope.posts = data;
             });
         };
@@ -156,49 +156,68 @@ angular.module('MatchCalendar', ['ui.bootstrap', 'ngCookies', 'ngSanitize', 'btf
 
     //service for fetching reddit posts from the JSON api
     .factory( 'RedditPostsService', ['$http', '$q', '$filter', 'MatchPost', function( $http, $q, $filter, MatchPost ) {
-        var uri = 'http://www.reddit.com/r/ultrahardcore/search.json?q=flair%3AUpcoming_Match&restrict_sr=on';
+        var uri = 'ultrahardcore/';
 
         return {
             //fetch all
-            query: function (limit, sort) {
-                var deferred = $q.defer();
-
+            query: function (subreddits, limit, sort) {
                 //set defaults
                 limit = limit || 100;
                 sort = sort || 'new';
 
-                //get the posts
-                $http.get(uri + '&limit=' + limit + '&sort=' + sort).then(
-                    function(data) {
+                var deferreds = [];
+                angular.forEach(subreddits, function(subreddit) {
+                    var deferred = $q.defer();
 
-                        //filter all the posts
-                        var unparsed = [];
-                        var parsed = [];
+                    var parsed = [];
+                    var unparsed = [];
+                    //get the posts
+                    $http.get('http://www.reddit.com/r/' + subreddit + '/search.json?q=flair%3AUpcoming_Match&restrict_sr=on&limit=' + limit + '&sort=' + sort).then(
+                        function(data) {
+                            angular.forEach(data.data.data.children, function(element) {
+                                //parse the post
+                                var matchPost = MatchPost.parseData(element.data);
 
-                        angular.forEach(data.data.data.children, function(element) {
-                            //parse the post
-                            var matchPost = MatchPost.parseData(element.data);
+                                if(null == matchPost) {
+                                    return;
+                                }
 
-                            if(null == matchPost) {
-                                return;
-                            }
+                                //if time was invalid push to the invalid stack
+                                matchPost.time == null ? unparsed.push(matchPost) : parsed.push(matchPost);
+                            });
+                            deferred.resolve({
+                                parsed: parsed,
+                                unparsed: unparsed
+                            });
+                        },
+                        function() {
+                            deferred.resolve({
+                                parsed: parsed,
+                                unparsed: unparsed
+                            });
+                        }
+                    );
+                    deferreds.push(deferred.promise);
+                });
 
-                            //if time was invalid push to the invalid stack
-                            matchPost.time == null ? unparsed.push(matchPost) : parsed.push(matchPost);
-                        });
+                var deferred = $q.defer();
+                $q.all(deferreds).then(function(data) {
+                    var parsed = [];
+                    var unparsed = [];
+                    angular.forEach(data, function(element) {
+                        parsed.push.apply(parsed, element.parsed);
+                        unparsed.push.apply(unparsed, element.unparsed);
+                    });
 
-                        //filter the parsed ones in time order
-                        var filteredParsed = $filter('orderBy')(parsed, function(element) {
-                            return element.time.format('X');
-                        });
+                    //filter the parsed ones in time order
+                    var filtered = $filter('orderBy')(parsed, function(element) {
+                        return element.time.format('X');
+                    });
 
-                        //add the unparsed matches to the end
-                        deferred.resolve(filteredParsed.concat(unparsed));
-                    },
-                    function(error) {
-                        deferred.reject(error);
-                    }
-                );
+                    //add the unparsed matches to the end
+                    filtered.push.apply(filtered, unparsed);
+                    deferred.resolve(filtered);
+                });
 
                 return deferred.promise;
             }

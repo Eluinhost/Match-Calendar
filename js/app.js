f427a392073977d02720e1524f4a13e9ca881910
Graham <graham_howden1@yahoo.co.uk>
2014-08-05 16:26:19 +0100

    allow use of the enter key for adding new subreddits

--- js/app.js
+++ js/app.js
@@ -418,4 +418,19 @@ angular.module('MatchCalendar', ['ui.bootstrap', 'ngCookies', 'ngSanitize', 'btf
                 }
             }
         }
-    }]);
+    }])
+
+    //directive with keybind="expression()" key=13
+    .directive('keybind', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === Number(attrs.key)) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.keybind);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });

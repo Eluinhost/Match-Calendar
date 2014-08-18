35ede8bca9265c92ab5a2f56af8365a2472d7b5b
Graham <graham_howden1@yahoo.co.uk>
2014-08-18 16:28:12 +0100

    improve grunt build file, now auto-injects bower dependencies, also concats + minifies all app and dependency js/css and uses that version

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,27 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-ng-annotate');
 
     grunt.initConfig({
         clean: {
             web: 'web',
             partials: 'web/partials/**/*.html',
             images: 'web/images',
-            php: 'web/**/*.php'
+            php: 'web/**/*.php',
+            tmp: '.tmp'
+        },
+        ngAnnotate: {
+            vendor: {
+                files: {
+                    '.tmp/concat/js/vendor.js': ['.tmp/concat/js/vendor.js']
+                }
+            }
         },
         copy: {
             php: {
@@ -17,10 +31,10 @@ module.exports = function(grunt) {
                 cwd: 'src/',
                 expand: true
             },
-            partials: {
+            html: {
                 src: [ '**/*.html' ],
-                dest: 'web/partials',
-                cwd: 'src/partials',
+                dest: 'web',
+                cwd: 'src',
                 expand: true
             },
             images: {
@@ -28,7 +42,57 @@ module.exports = function(grunt) {
                 dest: 'web/images',
                 cwd: 'src/images',
                 expand: true
+            },
+            fafonts: {
+                src: ['**/*'],
+                dest: 'web/fonts/',
+                cwd: 'bower_components/font-awesome/fonts',
+                expand: true
+            },
+            bsfonts: {
+                src: ['**/*'],
+                dest: 'web/fonts/',
+                cwd: 'bower_components/bootstrap/fonts',
+                expand: true
+            }
+        },
+        wiredep: {
+            deps: {
+                src: [ 'src/**/*.html' ],
+                exclude: [
+                    /.*bower_components.bootstrap.dist.js.bootstrap.js$/,
+                    /.*bower_components.jquery.dist.jquery.js$/
+                ]
+            }
+        },
+        useminPrepare: {
+            html: 'src/**/*.html',
+            options: {
+                dest: 'web'
+            }
+        },
+        usemin: {
+            html: ['web/**/*.html'],
+            options: {
+                assetsDirs: ['web']
             }
         }
     });
+
+    // simple build task
+    grunt.registerTask('build', [
+        'wiredep',
+        'useminPrepare',
+        'copy:html',
+        'copy:images',
+        'copy:php',
+        'copy:fafonts',
+        'copy:bsfonts',
+        'concat:generated',
+        'ngAnnotate:vendor',
+        'cssmin:generated',
+        'uglify:generated',
+        'usemin',
+        'clean:tmp'
+    ]);
 };

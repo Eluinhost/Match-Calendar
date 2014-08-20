9f617190d05a4843dd887f77a78cf147b2af439e
Graham <graham_howden1@yahoo.co.uk>
2014-08-20 22:08:21 +0100

    move keybind directive into its own file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -76,19 +76,4 @@ angular.module('matchCalendarApp', ['ui.bootstrap', 'ngCookies', 'ngSanitize', '
             });
 
         $urlRouterProvider.otherwise('/list');
-    }])
-
-    //directive with keybind="expression()" key=13
-    .directive('keybind', function() {
-        return function(scope, element, attrs) {
-            element.bind("keydown keypress", function(event) {
-                if(event.which === Number(attrs.key)) {
-                    scope.$apply(function(){
-                        scope.$eval(attrs.keybind);
-                    });
-
-                    event.preventDefault();
-                }
-            });
-        };
-    });
+    }]);

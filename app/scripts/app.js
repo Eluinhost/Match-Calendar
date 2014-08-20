28a3e1ce91baf98aa217c7568531bf72f8e37479
Graham <graham_howden1@yahoo.co.uk>
2014-08-20 21:24:50 +0100

    move notification time format into own file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,42 +78,6 @@ angular.module('matchCalendarApp', ['ui.bootstrap', 'ngCookies', 'ngSanitize', '
         $urlRouterProvider.otherwise('/list');
     }])
 
-    .factory('NotifcationTimeFormat', [function() {
-        return {
-            translateSeconds: function (duration) {
-                var hour = 0;
-                var min = 0;
-                var sec = 0;
-
-                if (duration) {
-                    if (duration >= 60) {
-                        min = Math.floor(duration / 60);
-                        sec = duration % 60;
-                    }
-                    else {
-                        sec = duration;
-                    }
-
-                    if (min >= 60) {
-                        hour = Math.floor(min / 60);
-                        min = min - hour * 60;
-                    }
-
-                    if (hour < 10) {
-                        hour = '0' + hour;
-                    }
-                    if (min < 10) {
-                        min = '0' + min;
-                    }
-                    if (sec < 10) {
-                        sec = '0' + sec;
-                    }
-                }
-                return hour + ":" + min + ":" + sec;
-            }
-        }
-    }])
-
     //a match post model
     .factory('MatchPost', ['MarkdownLinkDataService', '$rootScope', '$location', function (MarkdownLinkDataService, $rootScope, $location) {
 
---
+++ app/scripts/services/notificationtimeformat.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name matchCalendarApp.NotificationTimeFormat
+ * @description
+ * # NotificationTimeFormat
+ * Factory in the matchCalendarApp.
+ */
+angular.module('matchCalendarApp')
+    .factory('NotifcationTimeFormat', [function () {
+        return {
+            translateSeconds: function (duration) {
+                var hour = 0;
+                var min = 0;
+                var sec = 0;
+
+                if (duration) {
+                    if (duration >= 60) {
+                        min = Math.floor(duration / 60);
+                        sec = duration % 60;
+                    }
+                    else {
+                        sec = duration;
+                    }
+
+                    if (min >= 60) {
+                        hour = Math.floor(min / 60);
+                        min = min - hour * 60;
+                    }
+
+                    if (hour < 10) {
+                        hour = '0' + hour;
+                    }
+                    if (min < 10) {
+                        min = '0' + min;
+                    }
+                    if (sec < 10) {
+                        sec = '0' + sec;
+                    }
+                }
+                return hour + ":" + min + ":" + sec;
+            }
+        }
+    }]);

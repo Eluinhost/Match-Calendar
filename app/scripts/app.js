90c4dd25056f12348c35f99b6d9c2dcb5a5d6b1b
Graham <graham_howden1@yahoo.co.uk>
2014-08-20 22:07:13 +0100

    move date time picker into its own file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,44 +78,6 @@ angular.module('matchCalendarApp', ['ui.bootstrap', 'ngCookies', 'ngSanitize', '
         $urlRouterProvider.otherwise('/list');
     }])
 
-    .directive('dateTimePicker', [function() {
-        return {
-            restrict: 'AE',
-            scope: {
-                minDate: '=?',
-                pickedDate: '=',
-                meridian: '=',
-                timeZone: '='
-            },
-            templateUrl: 'views/dateTimePicker.html',
-            link: function($scope, $element, $attr) {
-                $scope.opened = false;
-
-                $scope.internalJSDate = $scope.pickedDate.toDate();
-                $scope.internalMinDate = $scope.minDate.toDate();
-
-                $scope.$watch('internalJSDate', function() {
-                    $scope.updatePickedDate();
-                });
-                $scope.$watch('timeZone', function() {
-                    $scope.updatePickedDate();
-                });
-
-                $scope.updatePickedDate = function() {
-                    var pickedMoment = moment($scope.internalJSDate);
-                    var formattedMoment = pickedMoment.format('MMM DD HH:mm');
-                    $scope.pickedDate = moment.tz(formattedMoment, 'MMM DD HH:mm', $scope.timeZone);
-                };
-
-                $scope.toggle = function($event) {
-                    $event.preventDefault();
-                    $event.stopPropagation();
-                    $scope.opened = !$scope.opened;
-                }
-            }
-        }
-    }])
-
     //directive with keybind="expression()" key=13
     .directive('keybind', function() {
         return function(scope, element, attrs) {

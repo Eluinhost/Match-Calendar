f86d26de5ccbd629735d7688c74fe6a1743b04e7
Graham <graham_howden1@yahoo.co.uk>
2014-08-20 22:05:31 +0100

    move date time service into its own file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,32 +78,6 @@ angular.module('matchCalendarApp', ['ui.bootstrap', 'ngCookies', 'ngSanitize', '
         $urlRouterProvider.otherwise('/list');
     }])
 
-    .factory('DateTimeService', ['$http', function($http) {
-        var resyncURL = 'php/sync.php';
-
-        return {
-            synced: false,
-            offset: null,
-            resync: function() {
-                var service = this;
-                $http.get(resyncURL).then(
-                    function(data) {
-                        service.synced = true;
-                        //this isn't really that accurate but within ping time so close enough
-                        service.offset = data.data.time - moment().valueOf();
-                    }
-                );
-            },
-            currentTime: function() {
-                var current = moment();
-                if (this.synced) {
-                    current.add('ms', this.offset);
-                }
-                return current;
-            }
-        }
-    }])
-
     .directive('dateTimePicker', [function() {
         return {
             restrict: 'AE',
---
+++ app/scripts/services/datetimeservice.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name matchCalendarApp.DateTimeService
+ * @description
+ * # DateTimeService
+ * Factory in the matchCalendarApp.
+ */
+angular.module('matchCalendarApp')
+    .factory('DateTimeService', ['$http', function ($http) {
+        var resyncURL = 'php/sync.php';
+
+        return {
+            synced: false,
+            offset: null,
+            resync: function () {
+                var service = this;
+                $http.get(resyncURL).then(
+                    function (data) {
+                        service.synced = true;
+                        //this isn't really that accurate but within ping time so close enough
+                        service.offset = data.data.time - moment().valueOf();
+                    }
+                );
+            },
+            currentTime: function () {
+                var current = moment();
+                if (this.synced) {
+                    current.add('ms', this.offset);
+                }
+                return current;
+            }
+        }
+    }]);

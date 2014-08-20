85a2bca2a4237f4f4528c994cf729af02518b528
Graham <graham_howden1@yahoo.co.uk>
2014-08-20 23:07:43 +0100

    prepare all html files, not just index

--- Gruntfile.js
+++ Gruntfile.js
@@ -216,7 +216,7 @@ module.exports = function (grunt) {
     // concat, minify and revision files. Creates configurations in memory so
     // additional tasks can operate on them
     useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
+      html: '<%= yeoman.app %>/**/*.html',
       options: {
         dest: '<%= yeoman.dist %>',
         flow: {

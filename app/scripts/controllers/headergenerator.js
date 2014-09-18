8a6e98eab200bbec1a8f82a38e197833401ebae3
Graham <graham_howden1@yahoo.co.uk>
2014-09-18 23:20:01 +0100

    add template section to the generator that can use variables and preview the response

--- app/scripts/controllers/headergenerator.js
+++ app/scripts/controllers/headergenerator.js
@@ -8,7 +8,7 @@
  * Controller of the matchCalendarApp
  */
 angular.module('matchCalendarApp')
-    .controller('HeadergeneratorCtrl', ['$scope', '$window', '$state', '$location', function ($scope, $window, $state, $location) {
+    .controller('HeadergeneratorCtrl', ['$scope', '$window', '$state', '$interpolate', function ($scope, $window, $state, $interpolate) {
         $scope.regions = {
             'AF': 'Africa',
             'AN': 'Antartica',
@@ -19,7 +19,15 @@ angular.module('matchCalendarApp')
             'SA': 'South America'
         };
 
-        $scope.generated = {
+        $scope.linkText = {
+            opens: '',
+            starts: '',
+            address: '',
+            title: '',
+            region: ''
+        };
+
+        $scope.templateVars = {
             opens: '',
             starts: '',
             address: '',
@@ -28,25 +36,36 @@ angular.module('matchCalendarApp')
         };
 
         $scope.$watch('opens', function (newValue) {
-            $scope.generated.opens = newValue.utc().format('YYYY-MM-DDTHH:mm:ssZ');
+            $scope.linkText.opens = newValue.utc().format('YYYY-MM-DDTHH:mm:ssZ');
+            $scope.templateVars.opens = newValue.utc().format('DD MMM HH:mm UTC');
             $scope.simpleUtcOpens = newValue.utc().format('YYYY-MM-DD HH:mm UTC');
         });
         $scope.$watch('starts', function (newValue) {
-            $scope.generated.starts = newValue.utc().format('YYYY-MM-DDTHH:mm:ssZ');
+            $scope.linkText.starts = newValue.utc().format('YYYY-MM-DDTHH:mm:ssZ');
+            $scope.templateVars.starts = newValue.utc().format('DD MMM HH:mm UTC');
             $scope.simpleUtcStarts = newValue.utc().format('YYYY-MM-DD HH:mm UTC');
         });
         $scope.$watch('address', function (newValue) {
-            $scope.generated.address = newValue.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
+            var address = newValue.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
+            $scope.linkText.address = address;
+            $scope.templateVars.address = address;
         });
         $scope.$watch('postTitle', function (newValue) {
-            $scope.generated.title = newValue.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
+            var title = newValue.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
+            $scope.linkText.title = title;
+            $scope.templateVars.title = title;
         });
         $scope.$watch('region', function (newValue) {
-            $scope.generated.region = newValue;
+            $scope.linkText.region = newValue;
+            $scope.templateVars.region = newValue;
         });
 
-        $scope.$watch('generated', function (newValue) {
+        $scope.$watch('linkText', function (newValue) {
             $scope.generatedLink = '[' + JSON.stringify(newValue) + '](/matchpost)';
+        }, true);
+
+        $scope.$watch('templateVars', function(newValue) {
+            $scope.updateTemplate();
         }, true);
 
         $scope.opens = $scope.timeOffset.currentTime();
@@ -54,6 +73,47 @@ angular.module('matchCalendarApp')
         $scope.address = '192.168.0.1';
         $scope.postTitle = 'Game Title';
         $scope.region = 'NA';
+
+        $scope.templating = {
+            raw: '**If you are new, be sure to read the [rules](RULES POST) and [Player FAQ](http://www.reddit.com/r/ultrahardcore/wiki/playerfaq)!**\n' +
+                '\n' +
+                '---\n' +
+                '\n' +
+                '***General Info***\n' +
+                '\n' +
+                'IP Address | Opens | Starts |\n' +
+                '-----------|--------|-------|\n' +
+                '{{ address }} | {{ opens }} | {{ starts }}\n' +
+                '\n' +
+                '---\n' +
+                '\n' +
+                '***Match Info***\n' +
+                '\n' +
+                '* **Golden Heads** - Enabled\n' +
+                '* **Absorption** - Disabled\n' +
+                '* **Towering** - Allowed\n' +
+                '* **Stealing** - Allowed\n' +
+                '* **Stalking** - Not Allowed\n' +
+                '* **PvP/iPvP** - 15 minutes in\n' +
+                '* **Map Size** - 2500x2500\n' +
+                '* **Game Length** - 90 minutes\n' +
+                '* **Nether** - Enabled\n' +
+                '* **Slots** - 60\n' +
+                '\n' +
+                '---\n' +
+                '\n' +
+                '    Winner: |\n' +
+                '---|\n' +
+                'TBA |\n' +
+                '\n' +
+                '***Scenario(s)***\n' +
+                '\n' +
+                '* Vanilla+ - Vanilla with a few minor changes'
+        };
+
+        $scope.updateTemplate = function() {
+            $scope.templating.parsed = $interpolate($scope.templating.raw, false, null, true)($scope.templateVars);
+        };
 
         $scope.sendToReddit = function() {
             $window.location.href = '/api/auth?callback=' + encodeURIComponent($state.href('auth', {}, {absolute: true}));

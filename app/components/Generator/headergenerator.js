b9f546d6927673a2bc64e8a3787ece42dae449dd
Graham <graham_howden1@yahoo.co.uk>
2014-10-13 21:52:44 +0100

    style(Generator): Remove all the old unused code

--- app/components/Generator/headergenerator.js
+++ app/components/Generator/headergenerator.js
@@ -19,36 +19,7 @@ angular.module('MatchCalendarApp')
             'SA': 'South America'
         };
 
-        $scope.generated = {
-            opens: '',
-            starts: '',
-            address: '',
-            title: '',
-            region: ''
-        };
-
         $scope.minTime = $scope.T.currentTime();
-
-        $scope.$watch('opens', function (newValue) {
-            $scope.generated.opens = newValue.utc().format('YYYY-MM-DDTHH:mm:ssZ');
-        });
-        $scope.$watch('starts', function (newValue) {
-            $scope.generated.starts = newValue.utc().format('YYYY-MM-DDTHH:mm:ssZ');
-        });
-        $scope.$watch('settings.generator.address', function (newValue) {
-            $scope.generated.address = newValue.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
-        });
-        $scope.$watch('settings.generator.postTitle', function (newValue) {
-            $scope.generated.title = newValue.replace(/\[/g, '&#91;').replace(/\]/g, '&#93;');
-        });
-        $scope.$watch('settings.generator.region', function (newValue) {
-            $scope.generated.region = newValue;
-        });
-
-        $scope.$watch('generated', function (newValue) {
-            $scope.generatedLink = '[' + JSON.stringify(newValue) + '](/matchpost)';
-        }, true);
-
         $scope.opens = $scope.T.currentTime();
         $scope.starts = $scope.T.currentTime();
 

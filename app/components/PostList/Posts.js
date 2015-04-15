e69fe580db37c8ee5a7908340f465ee4afe147a2
Graham <graham_howden1@yahoo.co.uk>
2015-04-15 21:30:46 +0100

    feat(Posts): support settings for team types
    refactor creating current lists + saving default to true for each type

--- app/components/PostList/Posts.js
+++ app/components/PostList/Posts.js
@@ -15,10 +15,14 @@ angular.module('MatchCalendarApp')
         $scope.lastUpdated = 0;
         $scope.regions = {};
         $scope.gamemodes = {};
+        $scope.teamTypes = {};
         $scope.currentRegions = [];
+        $scope.currentTeamTypes = [];
+        $scope.currentGamemodes = [];
         $localForage.bind($scope, 'lastUpdated');
         $localForage.bind($scope, 'regions');
         $localForage.bind($scope, 'gamemodes');
+        $localForage.bind($scope, 'teamTypes');
 
         $scope.updating = false;
         $scope.update = function() {
@@ -27,28 +31,9 @@ angular.module('MatchCalendarApp')
             RedditPostsService.query($rootScope.settings.subreddits, 200).then(function (data) {
                 $scope.posts = data;
                 $scope.updating = false;
-                $scope.currentRegions = [];
-                $scope.currentGamemodes = [];
-                var trackedLowercaseGamemodes = [];
-                angular.forEach($scope.posts, function (element) {
-                    element.region = element.region || 'Unknown';
-                    if($scope.currentRegions.indexOf(element.region) === -1) {
-                        $scope.currentRegions.push(element.region);
-                    }
-                    if (!angular.isDefined($scope.regions[element.region])) {
-                        $scope.regions[element.region] = true;
-                    }
-                    element.gamemodes.forEach(function(gamemode) {
-                        var lower = gamemode.toLowerCase();
-                        if (trackedLowercaseGamemodes.indexOf(lower) === -1) {
-                            trackedLowercaseGamemodes.push(lower);
-                            $scope.currentGamemodes.push(gamemode);
-                        }
-                        if (!angular.isDefined($scope.gamemodes[lower])) {
-                            $scope.gamemodes[lower] = true;
-                        }
-                    });
-                });
+                $scope.currentRegions = readRegions(data);
+                $scope.currentGamemodes = readGamemodes(data);
+                $scope.currentTeamTypes = readTeamTypes(data);
 
                 $scope.lastUpdated = $rootScope.T.currentTime().unix();
                 $rootScope.$broadcast('postsUpdated', $scope.posts);
@@ -64,6 +49,92 @@ angular.module('MatchCalendarApp')
 
         //watch for subreddit changes
         $rootScope.$watchCollection('settings.subreddits', $scope.update);
+
+        function defineFieldTrueIfNotExist(object, field) {
+            if (!angular.isDefined(object[field])) {
+                object[field] = true;
+            }
+        }
+
+        /**
+         * Removes duplicates from the array case insensitive
+         *
+         * @param {String[]} textArray
+         * @returns {String[]}
+         */
+        function createSetCaseInsensitive(textArray) {
+            return textArray.reduce(function(acc, text) {
+                var lower = text.toLowerCase();
+                if (acc.lower.indexOf(lower) === -1) {
+                    acc.lower.push(lower);
+                    acc.actual.push(text);
+                }
+                return acc;
+            }, {lower: [], actual: []})
+            .actual;
+        }
+
+        /**
+         * Fetches a list of all the regions in the post list. If a region is found we havn't seen before it's default
+         * saved setting will be initialized to true.
+         *
+         * @param {Array} posts
+         * @returns {Array}
+         */
+        function readRegions(posts) {
+            var regions = posts.map(function(post) { return post.region; });
+            var regionSet = createSetCaseInsensitive(regions);
+
+            // make sure they're saved
+            regionSet.forEach(function(region) {
+                defineFieldTrueIfNotExist($scope.regions, region.toLowerCase());
+            });
+
+            return regionSet;
+        }
+
+        /**
+         * Reads a list of all the gamemodes from the post list. If a gamemode is found we havn't seen before it's
+         * default save setting will be initialized to true
+         *
+         * @param {Array} posts
+         * @returns {Array}
+         */
+        function readGamemodes(posts) {
+            var gamemodes = posts.map(function(post) {
+                return post.gamemodes;
+            }).reduce(function(acc, gamemodes) {
+                gamemodes.forEach(function(gamemode) {
+                    acc.push(gamemode);
+                });
+                return acc;
+            }, []);
+
+            var gamemodeSet = createSetCaseInsensitive(gamemodes);
+            gamemodeSet.forEach(function(gamemode) {
+                defineFieldTrueIfNotExist($scope.gamemodes, gamemode.toLowerCase());
+            });
+            return gamemodeSet;
+        }
+
+        /**
+         * Reads a list of all the team types from the post list. If a team type is found we havn't seen before it's
+         * default save setting will be initialized to true
+         *
+         * @param {Array} posts
+         * @returns {Array}
+         */
+        function readTeamTypes(posts) {
+            var teamTypes = posts.map(function(post) { return post.teams; });
+            var teamTypeSet = createSetCaseInsensitive(teamTypes);
+
+            // make sure they're saved
+            teamTypeSet.forEach(function(teamType) {
+                defineFieldTrueIfNotExist($scope.teamTypes, teamType.toLowerCase());
+            });
+
+            return teamTypeSet;
+        }
 
         // Public return value
         return $scope;

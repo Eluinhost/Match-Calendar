1ca5ecb39df8fae8ede35724ccbd046a1f28bd94
Graham <graham_howden1@yahoo.co.uk>
2015-04-23 23:44:18 +0100

    feat(TemplateEditor): UI for editing and managing multiple post templates. Missing validation of template names to avoid duplication

--- app/components/TemplateEditor/TemplateEditor.js
+++ app/components/TemplateEditor/TemplateEditor.js
@@ -8,8 +8,74 @@
  * Controller of the MatchCalendarApp
  */
 angular.module('MatchCalendarApp')
-    .controller('TemplateEditorCtrl', ['$scope', 'Templates', function ($scope, Templates) {
-        $scope.editing = null;
+    .controller('TemplateEditorCtrl', ['$scope', '$interpolate', 'Templates', function ($scope, $interpolate, Templates) {
+        $scope.templates = Templates;
 
+        $scope.loadNew = function(template) {
+            if (template) {
+                // editing existing
+                $scope.tempCopy = angular.copy(template);
+                $scope.selected = template;
+            } else {
+                // new template
+                $scope.tempCopy = {
+                    name: '',
+                    template: ''
+                };
+                $scope.selected = null;
+            }
 
+            // force refresh
+            $scope.updatePreview();
+        };
+
+        // updates the preview window on change of the template
+        $scope.updatePreview = function() {
+            if (!$scope.tempCopy) {
+                $scope.generated = '';
+                return;
+            }
+
+            $scope.generated = $interpolate($scope.tempCopy.template, false, null, true)($scope);
+        };
+
+        // TODO check name conflicts
+        // saves the template to the templates service
+        $scope.saveTemplate = function() {
+            if ($scope.selected) {
+                $scope.selected.name = $scope.tempCopy.name;
+                $scope.selected.template = $scope.tempCopy.template;
+            } else {
+                Templates.customTemplates.push($scope.tempCopy);
+            }
+
+            // reset the view to avoid problems with sync
+            $scope.reset();
+        };
+
+        // deletes a template without warning
+        $scope.deleteTemplate = function() {
+            var index = -1;
+            for (var i = 0; i < Templates.customTemplates.length; i++) {
+                if (Templates.customTemplates[i].name === $scope.selected.name) {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index === -1) return;
+
+            Templates.customTemplates.splice(index, 1);
+            $scope.reset();
+        };
+
+        // reset the view to normal
+        $scope.reset = function() {
+            $scope.selected = null;
+            $scope.tempCopy = null;
+            $scope.preview = '';
+        };
+
+        // initialize
+        $scope.reset();
     }]);
--- app/components/TemplateEditor/Templates.js
+++ app/components/TemplateEditor/Templates.js
@@ -11,8 +11,9 @@ angular.module('MatchCalendarApp')
     .factory('Templates', ['$rootScope', '$localForage', function ($rootScope, $localForage) {
         var $scope = $rootScope.$new(true);
 
-        $scope.templates = {};
-        $localForage.bind($scope, 'templates');
+        $scope.customTemplates = [];
 
-        return $scope.templates;
+        $localForage.bind($scope, 'customTemplates');
+
+        return $scope;
     }]);

8e299b4ef3cff799159b8a0cb2b95f3c56a05c0a
Graham <graham_howden1@yahoo.co.uk>
2015-04-28 11:57:22 +0100

    feat(Templates): add way to get template by it's name, add way to compile a template with a scope

--- app/components/TemplateEditor/Templates.js
+++ app/components/TemplateEditor/Templates.js
@@ -8,19 +8,29 @@
  * Service of the MatchCalendarApp
  */
 angular.module('MatchCalendarApp')
-    .factory('Templates', ['$rootScope', '$localForage', function ($rootScope, $localForage) {
+    .factory('Templates', ['$rootScope', '$localForage', '$interpolate', function ($rootScope, $localForage, $interpolate) {
         var $scope = $rootScope.$new(true);
 
         $scope.customTemplates = [];
 
         $scope.customTemplateExists = function(name) {
+            return !!$scope.getTemplate(name);
+        };
+
+        $scope.getTemplate = function(name) {
             for (var i = 0; i < $scope.customTemplates.length; i++) {
                 if ($scope.customTemplates[i].name === name) {
-                    return true;
+                    return name;
                 }
             }
+        };
 
-            return false;
+        $scope.compileTemplate = function(name, scope) {
+            var template = $scope.getTemplate(name);
+
+            if (!template) return '';
+
+            return $interpolate(template.template, false, null, true)(scope);
         };
 
         $localForage.bind($scope, 'customTemplates');

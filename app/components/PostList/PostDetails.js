4d364c701b674f686c5a0c9726f252ff9ff022cf
Graham <graham_howden1@yahoo.co.uk>
2015-05-06 19:59:04 +0100

    feat(PostDetails): add new directive for showing post details

---
+++ app/components/PostList/PostDetails.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name MatchCalendarApp.directive:PostDetails
+ * @description
+ * # PostDetails
+ */
+angular.module('MatchCalendarApp')
+    .directive('postDetails', ['DateTimeService', 'PostNotifications', function (DateTimeService, PostNotifications) {
+        return {
+            restrict: 'E',
+            scope: {
+                post: '='
+            },
+            templateUrl: 'components/PostList/PostDetails.html',
+            link: function ($scope) {
+                console.log($scope);
+                $scope.DateTime = DateTimeService;
+                $scope.Notifications = PostNotifications;
+
+                $scope.toggleNotification = function() {
+                    PostNotifications.toggleNotifications($scope.post.id);
+                };
+
+                $scope.isNotifying = function() {
+                    return PostNotifications.isNotifyingFor($scope.post.id);
+                };
+            }
+        };
+    }]);

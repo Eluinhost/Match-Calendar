6afb7d4e82f07a52873974b5a60a1c435da2822c
Graham <graham_howden1@yahoo.co.uk>
2015-06-10 11:22:23 +0100

    feat(PostGenerator): change to new proposal for game type using just team sizes (except for FFA)

--- app/components/Generator/PostGeneratorGameType.js
+++ app/components/Generator/PostGeneratorGameType.js
@@ -15,16 +15,19 @@ angular.module('MatchCalendarApp')
          *
          * @param {Object} options
          * @param {String} options.name - the name of the game mode
+         * @param {String} options.shortCode - the code to put in the shortened version
+         * @param {PostGeneratorGameTypeField[]} [options.fields] - list of fields needed for the gamemode, default to
+         *                                                          just team size field
          * @param {String} options.description - a short description of the game mode
-         * @param {PostGeneratorGameTypeField[]} [options.fields] - list of fields needed for the gamemode
-         * @param {Function} [options.format] - optional formatter to override the default
+         * @param {Function} [options.format] - optional formatter to override the default of shortCode + 'To' + size
          * @constructor
          */
         var GameType = function(options) {
             this.name = options.name;
+            this.shortCode = options.shortCode;
             this.description = options.description || 'No description set';
-            this.fields = options.fields || [];
-            this.format = options.format || function() { return 'ERROR CREATING GAME TYPE'; };
+            this.fields = options.fields || [PostGeneratorGameTypeField.TEAM_SIZE];
+            this.format = options.format || genericSizeFormatter;
         };
 
         GameType.prototype.defaultValues = function() {
@@ -37,14 +40,14 @@ angular.module('MatchCalendarApp')
 
         // generic formatter to use with size based game types
         var genericSizeFormatter = function(fieldAnswers) {
-            return this.name + ' To' + fieldAnswers.size;
+            return this.shortCode + 'To' + fieldAnswers.size;
         };
-
 
         GameType.types = {};
 
         GameType.types.FFA = new GameType({
             name: 'FFA',
+            shortCode: 'FFA',
             description: 'Every man for himself.',
             fields: [],
             format: function() {
@@ -54,95 +57,32 @@ angular.module('MatchCalendarApp')
 
         GameType.types.CHOSEN = new GameType({
             name: 'Chosen',
-            description: 'Players form their teams before the match.',
-            fields: [PostGeneratorGameTypeField.TEAM_SIZE],
-            format: genericSizeFormatter
+            shortCode: 'C',
+            description: 'Players form their teams before the match.'
         });
 
         GameType.types.RANDOM = new GameType({
             name: 'Random',
-            description: 'Players are assigned random teammates.',
-            fields: [PostGeneratorGameTypeField.TEAM_SIZE],
-            format: genericSizeFormatter
+            shortCode: 'R',
+            description: 'Players are assigned random teammates.'
         });
 
         GameType.types.PICKED = new GameType({
             name: 'Picked',
-            description: 'Players take turns joining a team.',
-            fields: [PostGeneratorGameTypeField.TEAM_SIZE],
-            format: genericSizeFormatter
-        });
-
-        GameType.types.MYSTERY = new GameType({
-            name: 'Mystery',
-            description: 'Players are assigned unknown teammates.',
-            fields: [PostGeneratorGameTypeField.TEAM_SIZE],
-            format: genericSizeFormatter
+            shortCode: 'P',
+            description: 'Players take turns joining a team.'
         });
 
         GameType.types.CAPTAINS = new GameType({
             name: 'Captains',
-            description: 'Selected team leaders pick their teammates.',
-            fields: [
-                new PostGeneratorGameTypeField({
-                    question: 'Number of captains',
-                    defaultValue: 4,
-                    type: 'number',
-                    name: 'captains'
-                }),
-                new PostGeneratorGameTypeField({
-                    question: 'Selection Method',
-                    defaultValue: '',
-                    name: 'selection'
-                })
-            ],
-            format: function(fieldAnswers) {
-                return fieldAnswers.captains + ' Captains ' + fieldAnswers.selection;
-            }
+            shortCode: 'Cpt',
+            description: 'Selected team leaders pick their teammates.'
         });
 
         GameType.types.AUCTION = new GameType({
             name: 'Auction',
-            description: 'Selected team leaders buy their teammates.',
-            fields: [
-                new PostGeneratorGameTypeField({
-                    question: 'Number of buyers',
-                    defaultValue: 4,
-                    type: 'number',
-                    name: 'buyers'
-                }),
-                new PostGeneratorGameTypeField({
-                    question: 'Currency',
-                    defaultValue: 'diamonds',
-                    type: 'text',
-                    name: 'currency'
-                })
-            ],
-            format: function(fieldAnswers) {
-                return fieldAnswers.buyers + ' buyers Auction (' + fieldAnswers.currency + ')';
-            }
-        });
-
-        GameType.types.ALLIANCES = new GameType({
-            name: 'Alliance',
-            description: 'Players can form informal teams on their own.',
-            fields: [
-                new PostGeneratorGameTypeField({
-                    question: 'Max amount of players for an alliance',
-                    defaultValue: 8,
-                    type: 'number',
-                    name: 'max'
-                }),
-                new PostGeneratorGameTypeField({
-                    question: 'Can alliances win?',
-                    defaultValue: true,
-                    type: 'checkbox',
-                    name: 'canWin'
-                })
-            ],
-            format: function(fieldAnswers) {
-                return 'Alliances up to ' + fieldAnswers.max + '. Alliances can' + (fieldAnswers.canWin ? '' : 'not') + ' win';
-            }
+            shortCode: 'Auc',
+            description: 'Selected team leaders buy their teammates.'
         });
 
         GameType.types.CUSTOM = new GameType({

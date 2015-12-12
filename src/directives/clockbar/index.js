4c623642887c4387b408ae91c2e9439ba54aede6
Eluinhost <graham_howden1@yahoo.co.uk>
2015-12-12 16:25:19 +0000

    feat(clockbar): add the time difference between client and server to a tooltip on the clock

--- src/directives/clockbar/index.js
+++ src/directives/clockbar/index.js
@@ -1,15 +1,23 @@
 import template from './template.html';
 
-function clockbar(DateTime) {
+function clockbar(DateTime, DurationFormatter) {
     return {
         restrict: 'E',
         scope: {},
         template: template,
         link: function(scope) {
             scope.DateTime = DateTime;
+
+            scope.tooltip = function() {
+                if (DateTime.synced) {
+                    return 'Time difference to the server: ' + DurationFormatter.format(DateTime.offset / 1000);
+                }
+
+                return 'Time has not been synced yet';
+            };
         }
     };
 }
-clockbar.$inject = ['DateTime'];
+clockbar.$inject = ['DateTime', 'DurationFormatter'];
 
 export default clockbar;

775f17eeec4bff7a3c6d544258f0d6dec0361c13
Eluinhost <graham_howden1@yahoo.co.uk>
2015-12-13 17:57:37 +0000

    feat(notInArray): validation directive for unique in array

--- src/directives/notInArray/index.js
+++ src/directives/notInArray/index.js
@@ -1,15 +1,18 @@
-/**
- * @ngdoc directive
- * @name directive:notInArray
- * @description
- *
- * Directive for validating a ng-model value
- */
-function keybind() {
-    return function(scope, element, attrs) {
-        // TODO validation based on provided array and contains
+import _ from 'lodash';
+
+function notInArray() {
+    return {
+        require: 'ngModel',
+        scope: {
+            notInArray: '='
+        },
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$validators.notInArray = function(modelValue) {
+                return !_.contains(scope.notInArray, modelValue);
+            };
+        }
     };
 }
-keybind.$inject = [];
+notInArray.$inject = [];
 
-export default keybind;
+export default notInArray;

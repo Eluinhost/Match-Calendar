531db09b5c30dc07d2857ae54c1c2e1ff8c6a265
Eluinhost <graham_howden1@yahoo.co.uk>
2015-12-17 21:00:56 +0000

    feat(notInArray): add support for flagging as case insensitive

--- src/directives/notInArray/index.js
+++ src/directives/notInArray/index.js
@@ -4,11 +4,17 @@ function notInArray() {
     return {
         require: 'ngModel',
         scope: {
-            notInArray: '='
+            notInArray: '=',
+            notInArrayCaseSensitive: '=?'
         },
         link: function(scope, elm, attrs, ctrl) {
             ctrl.$validators.notInArray = function(modelValue) {
-                return !_.contains(scope.notInArray, modelValue);
+                if (scope.notInArrayCaseSensitive) {
+                    return !_.contains(scope.notInArray, modelValue);
+                }
+
+                let check = modelValue.toLowerCase();
+                return !_.any(scope.notInArray, item => item.toLowerCase() === check);
             };
         }
     };

e4ad99a0c8a48ce72067558498aac52d0abf4deb
Eluinhost <graham_howden1@yahoo.co.uk>
2015-12-17 21:23:55 +0000

    feat(uniqueItemEditor): directive for editing a unique list of items with input/removal with feedback
    updated to angular 1.5.0-rc.0

---
+++ src/directives/uniqueItemsEditor/index.js
@@ -0,0 +1,39 @@
+class UniqueItemsEditorCtrl {
+    constructor() {
+        this.tempValue = this.tempValue || '';
+        this.placeholder = this.placeholder || '';
+        this.uniqueItems = this.uniqueItems || [];
+    }
+
+    addNew(valid) {
+        if (!valid) {
+            return;
+        }
+
+        this.uniqueItems.push(this.tempValue);
+        this.tempValue = '';
+    }
+}
+
+function unqiueItemsEditor() {
+    return {
+        scope: {
+            uniqueItems: '=',
+            tempValue: '=?',
+            buttonPrefix: '@?',
+            buttonIcon: '@',
+            placeholder: '@?'
+        },
+        transclude: {
+            requiredError: '?requiredError',
+            inArrayError: '?'
+        },
+        template: require('./template.html'),
+        controller: UniqueItemsEditorCtrl,
+        controllerAs: 'unique',
+        bindToController: true
+    };
+}
+unqiueItemsEditor.$inject = [];
+
+export default unqiueItemsEditor;

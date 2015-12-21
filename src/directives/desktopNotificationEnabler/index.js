eb8d8fec7db0b65f84ee06b5b277c34dc5dcbaa8
Eluinhost <graham_howden1@yahoo.co.uk>
2015-12-21 20:18:38 +0000

    feat(desktopNotificationEnabler): allow showing the close button/dismissing the alert

--- src/directives/desktopNotificationEnabler/index.js
+++ src/directives/desktopNotificationEnabler/index.js
@@ -1,6 +1,27 @@
+import _ from 'lodash';
+
+const HIDE_KEY = 'hideNotificationsMessage';
+
 class NotificationEnablerCtrl {
-    constructor(HtmlNotifications) {
+    constructor(HtmlNotifications, $localForage, $rootScope) {
         this.HtmlNotifications = HtmlNotifications;
+        this.hide = false;
+        this.intiialised = false;
+
+        $localForage
+            .getItem(HIDE_KEY)
+            .then(hide => {
+                if (!_.isNull(hide)) {
+                    this.hide = hide;
+                }
+
+                this.initialised = true;
+                $rootScope.$watch(() => this.hide, () => $localForage.setItem(HIDE_KEY, this.hide));
+            });
+    }
+
+    show() {
+        return this.initialised && (this.hide === false || this.showHideButton === false);
     }
 
     requestPermission() {
@@ -9,13 +30,15 @@ class NotificationEnablerCtrl {
             .then(() => this.HtmlNotifications.notify('Notifications Enabled!'));
     }
 }
-NotificationEnablerCtrl.$inject = ['HtmlNotifications'];
+NotificationEnablerCtrl.$inject = ['HtmlNotifications', '$localForage', '$rootScope'];
 
 function desktopNotificationEnabler() {
     return {
         restrict: 'E',
         template: require('./template.html'),
-        scope: {},
+        scope: {
+            showHideButton: '=?'
+        },
         controller: NotificationEnablerCtrl,
         controllerAs: 'enabler',
         bindToController: true
